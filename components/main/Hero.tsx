e1b44befc7110c8b634abbce176cd9579d863088
hemprasad747 <153615973+hemprasad747@users.noreply.github.com>
2023-12-14 02:22:06 +0530

    xx

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,4 +1,3 @@
-// components/main/Hero.tsx
 import React from "react";
 import HeroContent from "../sub/HeroContent";
 import Image from "next/image";
@@ -6,15 +5,16 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <div className="relative flex flex-col h-full w-full" id="about-me">
-      {/* Replace the video with the Image component */}
-      <Image
-        src="/cyber6.png"  // Replace with the correct path to your image
-        alt="Cyber Image"
-        layout="fill"
-        objectFit="cover"
-        style={{ opacity: 0.5 }}  {/* Set opacity to half */}
-        className="rotate-0 absolute top-[-340px] left-0 z-[1]"
-      />
+      <div className="relative">
+        <Image
+          src="/cyber6.png"  
+          alt="Cyber Image"
+          layout="fill"
+          objectFit="cover"
+          className="rotate-0 absolute top-[-340px] left-0 z-[1]"
+        />
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+      </div>
       <HeroContent />
     </div>
   );
