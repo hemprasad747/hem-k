ce8ff20f935899492e7d19c919e8a2b5092601fc
hemprasad747 <153615973+hemprasad747@users.noreply.github.com>
2023-12-14 03:05:50 +0530

    f

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,51 +1,33 @@
-// ./app/layout.tsx
-
+// layout.jsx
 import React from 'react';
-import { css } from '@emotion/styled';
-import { Inter } from 'next/font/google';
-import { Metadata } from 'next';
-import StarsCanvas from '@/components/main/StarBackground';
-import Navbar from '@/components/main/Navbar';
-import Footer from '@/components/main/Footer';
+import "./globals.css"; // Adjust the path based on your actual structure
+import { Inter } from "next/font/google";
+import { Metadata } from "next";
+import StarsCanvas from "@/components/main/StarBackground";
+import Navbar from "@/components/main/Navbar";
+import Footer from "@/components/main/Footer";
 
-const inter = Inter({ subsets: ['latin'] });
-
+const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
-  title: 'universe Portfolio',
-  description: 'This is my portfolio',
+  title: "universe Portfolio",
+  description: "This is my portfolio",
 };
 
-const containerStyles = css`
-  /* Common styles for all screen sizes */
-
-  /* Media query for screens smaller than 600px */
-  @media only screen and (max-width: 600px) {
-    /* Styles for small screens go here */
-  }
-
-  /* Media query for screens between 600px and 1200px */
-  @media only screen and (min-width: 601px) and (max-width: 1200px) {
-    /* Styles for medium screens go here */
-  }
-
-  /* Media query for screens larger than 1200px */
-  @media only screen and (min-width: 1201px) {
-    /* Styles for large screens go here */
-  }
-`;
-
-const RootLayout: React.FC = ({ children }) => (
-  <html lang="en">
-    <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden ${containerStyles}`}>
-      <StarsCanvas />
-      <Navbar />
-      {children}
-      <Footer />
-      <style jsx global>{`
-        /* Your global styles go here */
-      `}</style>
-    </body>
-  </html>
-);
-
-export default RootLayout;
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+      >
+        <StarsCanvas />
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
