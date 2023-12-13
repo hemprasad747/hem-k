93e494e8e7f2cb43915f9f1cfa76af1b40f8c087
hemprasad747 <153615973+hemprasad747@users.noreply.github.com>
2023-12-14 01:34:29 +0530

    updated

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -7,11 +7,11 @@ const Hero = () => {
     <div className="relative flex flex-col h-full w-full" id="about-me">
       {/* Replace the video with the Image component */}
       <Image
-        src="/cyber6.png"  // Replace with the correct path to your image
+        src="/cyber6.png"  
         alt="Cyber Image"
         layout="fill"
         objectFit="cover"
-        opacity=""
+        opacity="0.5"
         className="rotate-0 absolute top-[-340px] left-0 z-[1]"
       />
       <HeroContent />
